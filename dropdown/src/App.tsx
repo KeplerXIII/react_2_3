b6dcb076f297ed8c4023fde98f7dd8f396f65576
Alexander Pushkin (Home) <a.v.pushkin@gmail.com>
2023-11-22 17:50:40 +0300

    upd

--- dropdown/src/App.tsx
+++ dropdown/src/App.tsx
@@ -1,41 +1,10 @@
-import { useState } from 'react'
 import './App.css'
-
-const DropdownList = ['Profile', 'Settings', 'Log Out']
+import Dropdown from './components/dropdow'
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false)
-  const [chosenItem, setItem] = useState(-1)
-
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  }
-
-  function setChosen (index: number) {
-    console.log(index)
-    setItem(index)
-  }
-
+  
   return (
-    <div className="container">
-            <div className={`dropdown-wrapper ${isOpen ? 'open' : ''}`}>
-                <div className="btn" onClick={toggleDropdown}>
-                    Dropdown <i className="material-icons">arrow_drop_down</i>
-                </div>
-                <ul className="dropdown">
-                    {DropdownList.map((obj, index) => <li 
-                      key={index} 
-                      onClick={() => setChosen(index)}
-                      style={{
-                        backgroundColor: chosenItem === index ? '#5380F7' : 'transparent',
-                      }}
-                      >
-                        <a>
-                        {obj}
-                        </a></li>)}
-                </ul>
-            </div>
-        </div>
+    <Dropdown/>
   )
 }
 
--- dropdown/src/components/dropdow.tsx
+++ dropdown/src/components/dropdow.tsx
@@ -1,16 +1,41 @@
-export function DropDown () {
-    return (
-        <div className="container">
-            <div className="dropdown-wrapper">
-                <div className="btn">
+import { useState } from 'react'
+
+const DropdownList = ['Profile', 'Settings', 'Log Out']
+
+function Dropdown() {
+  const [isOpen, setIsOpen] = useState(false)
+  const [chosenItem, setItem] = useState(-1)
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  }
+
+  function setChosen (index: number) {
+    console.log(index)
+    setItem(index)
+  }
+
+  return (
+    <div className="container">
+            <div className={`dropdown-wrapper ${isOpen ? 'open' : ''}`}>
+                <div className="btn" onClick={toggleDropdown}>
                     Dropdown <i className="material-icons">arrow_drop_down</i>
                 </div>
                 <ul className="dropdown">
-                    <li><a>Item 1</a></li>
-                    <li><a>Item 2</a></li>
-                    <li><a>Item 3</a></li>
+                    {DropdownList.map((obj, index) => <li 
+                      key={index} 
+                      onClick={() => setChosen(index)}
+                      style={{
+                        backgroundColor: chosenItem === index ? '#5380F7' : 'transparent',
+                      }}
+                      >
+                        <a>
+                        {obj}
+                        </a></li>)}
                 </ul>
             </div>
         </div>
-    )
+  )
 }
+
+export default Dropdown
